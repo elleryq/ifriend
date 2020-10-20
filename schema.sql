281b6a99a005672da8e01212bb01329abeef7713
Yan-ren Tsai <elleryq@gmail.com>
2020-10-20 16:00:30 +0800

    Adjust schema for user profile

--- schema.sql
+++ schema.sql
@@ -1,7 +1,18 @@
+CREATE TABLE profiles (
+    id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
+    username TEXT NOT NULL,
+    name TEXT NOT NULL,
+    bio TEXT,
+    interest TEXT,
+    picture TEXT
+);
+
 CREATE TABLE users (
     id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
     email TEXT NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    profile_id INTEGER
 );
 
 CREATE INDEX idx_email ON users (email);
+
